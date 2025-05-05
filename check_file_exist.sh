160a669c6a10c42a7669297d5f24d61bf5eb9b51
ramanesdr <ashaougui@gmail.com>
2025-05-05 16:53:55 +0100

    first commit

---
+++ check_file_exist.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+read -p "Entrez le nom du fichier : " filename
+
+
+if [ -f "$filename" ]; then
+	echo "Le fichier '$filename' existe"
+else
+	echo "Le fichier '$filename' n'existe pas"
+fi
